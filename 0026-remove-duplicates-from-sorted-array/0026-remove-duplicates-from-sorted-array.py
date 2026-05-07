3d1dbe98ede509adc0550625e032f22e23a4d482
Harshajny <75127170+Harshajny@users.noreply.github.com>
2026-05-08 02:12:19 +0530

    Time: 0 ms (100%), Space: 20.6 MB (16.68%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,19 +1,13 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        
-        # 'k' is our slow pointer, starting at the second element
-        k = 1 
-        
-        # 'i' is our fast pointer, scanning the rest of the array
-        for i in range(1, len(nums)):
-            # We want to check if the current element is different 
-            # from the one right before it.
-            if nums[i] != nums[i - 1]:
-                # If it's unique, we "write" it at position 'k'
-                nums[k] = nums[i]
-                # Then move 'k' forward
-                k += 1
-        
-        return k
+        x=1
+        for i in range(len(nums)-1):
+            if(nums[i]!=nums[i+1]):
+                nums[x]=nums[i+1]
+                x=x+1
+                
+        return x
+
+#no need of sets as they have said its inplace and sorted, we could use 2 pointers
+                      
+       
