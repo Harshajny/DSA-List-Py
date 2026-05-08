d10b8b0a33919bade6c21953ba4848243ef05ef1
Harshajny <75127170+Harshajny@users.noreply.github.com>
2026-05-08 17:03:46 +0530

    Time: 2 ms (85.82%) | Memory: 20.5 MB (23.37%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -7,15 +7,4 @@ class Solution:
                x += 1
                 
  
-        # x tracks where the next non-zero should be placed
-        x = 0
-        
-        for i in range(len(nums)):
-            # If we find a non-zero number
-            if nums[i] != 0:
-                # Swap it with the element at position x
-                nums[x], nums[i] = nums[i], nums[x]
-                
-                # Increment x to the next available spot
-                x += 1
-        
+       
